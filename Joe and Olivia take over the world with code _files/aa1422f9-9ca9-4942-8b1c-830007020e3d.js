b617d36aa48517cf3bb93bb29b541c2c686ba91a
Joe_Biesemeyer <joebieseneyer@gmail.com>
2019-02-02 13:51:41 -0800

    add workshop project to git

---
+++ Joe and Olivia take over the world with code _files/aa1422f9-9ca9-4942-8b1c-830007020e3d.js
@@ -0,0 +1,8 @@
+!function(){
+window.initialUsersnapClassicGetConfig={"apiKey": "aa1422f9-9ca9-4942-8b1c-830007020e3d", "locale": "auto", "fields": {"comment": {"required": true}, "assignee": null, "email": {"required": false}, "label": null}, "button": {"position": "bottomRight", "label": "FEEDBACK"}, "shortcut": null, "apiAccess": "basic", "colors": {"primary": "#005E99", "secondary": "#66AACB"}, "serviceUrl": "https://api.usersnap.com", "consoleRecorder": false};
+var s=document.createElement('script'),n=document.getElementsByTagName('script')[0],p="//cdn.usersnap.com/classic/frontend";
+s.type='text/javascript';
+s.async=!0;
+s.src=p+"/widget/loader.js";
+n.parentNode.insertBefore(s,n);
+}();
